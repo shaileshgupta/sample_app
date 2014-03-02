95750cd36d399600970a4e2d1275a465109b7da2
Shailesh Gupta <contact.shaileshgupta@gmail.com>
2014-03-02 10:43:11 +0530

    finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
 
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = md5_hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  def md5_hexdigest(data)
+    Digest::MD5::hexdigest(data)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +9,18 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_secure_password
+
+  def self.sha1_hexdigest(data)
+    Digest::SHA1.hexdigest(data.to_s)
+  end
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.sha1_hexdigest(User.new_remember_token)
+    end
 end
